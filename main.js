b548401e001716e92cc6e292f097c04deff99a5e
Nicolás López <nicolaslopezj@me.com>
2016-03-14 18:30:16 -0300

    support for folders

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 
 var fs = require('fs');
 var AWS = require('aws-sdk');
+var mime = require('mime');
+var clc = require('cli-color');
+var ProgressBar = require('progress');
+var _ = require('underscore');
+var rmdir = require('rimraf');
 
 // CLI Options
 var program = require('commander');
@@ -21,6 +26,7 @@ var meteor = require('./meteor.js');
 program
   .version(packageJson.version)
   .usage('[options]')
+  .option('-d, --deploy <deploy.json>', 'The path of the deploy information file.')
   .option('-p, --path <path>', 'The path used to link the files, default is "/", pass "" to link relative to the index.html.')
   .option('-t, --template <file path>', 'Provide a custom index.html template. Use {{> head}}, {{> css}} and {{> scripts}} to place the meteor resources.')
   .option('-s, --settings <settings.json>', 'Set optional data for Meteor.settings in your application.')
@@ -43,7 +49,7 @@ if (!argPath) {
 
   (function() {
 
-    // check if in meteor folder
+    // Check if in meteor folder
     try {
       if (!fs.lstatSync('./.meteor').isDirectory())
         throw new Error();
@@ -53,7 +59,7 @@ if (!argPath) {
       return;
     }
 
-    // check template file
+    // Check template file
     if (program.template) {
       try {
         if (!fs.lstatSync(program.template).isFile())
@@ -67,44 +73,47 @@ if (!argPath) {
 
     var deployOptions = null;
     try {
-      var opts = fs.readFileSync('deploy.json', 'utf8');
+      var opts = fs.readFileSync(program.deploy || 'deploy.json', 'utf8');
       deployOptions = JSON.parse(opts);
     } catch (e) {
       console.error('Error reading deploy options', e);
       return;
     }
 
-    // build meteor
+    // Build meteor
     queue.add(function(callback) {
-      console.log('Bundling Meteor app...');
+      console.log('');
+      console.log(clc.blue.underline('Deploy S3: Meteor Apps in AWS S3'));
+      console.log('');
+      console.log(clc.bold('Creating bundle...'));
       meteor.build(program, callback);
     });
 
-    // move the files into the build folder
+    // Move the files into the build folder
     queue.add(function(callback) {
-      console.log('Generating the index.html...');
       meteor.move(callback);
     });
 
-    // create the index.html
+    // Create the index.html
     queue.add(function(callback) {
       meteor.addIndexFile(program, callback);
     });
 
-    // delete unecessary fiels
+    // Delete unecessary fiels
     queue.add(function(callback) {
       meteor.cleanUp(function() {
         console.log('Bundle created');
+        console.log('');
         callback();
       });
     });
 
-    AWS.config.loadFromPath('deploy.json');
+    AWS.config.loadFromPath(program.deploy || 'deploy.json');
     var s3 = new AWS.S3();
 
     queue.add(function(callback) {
-      console.log('Starting deployment...');
-      console.log('Deleting old files...');
+      console.log(clc.bold('Starting deployment...'));
+      console.log('Deleting old files in bucket...');
       s3.listObjects({ Bucket: deployOptions.bucket }, function(err, data) {
         if (err) return console.log(err.message);
 
@@ -129,59 +138,95 @@ if (!argPath) {
       });
     });
 
-    // upload
+    var bar;
+    var lastProgress = 0;
+    var printFileProgress = function(fileList, fileName) {
+      var list = _.compact(fileList);
+      var total = _.reduce(list, (memo, item) => item.total + memo, 0);
+      var loaded = _.reduce(list, (memo, item) => item.loaded + memo, 0);
+
+      if (!bar) {
+        bar = new ProgressBar('[:bar] :percent', { total: total, width: 50 });
+      } else {
+        if (!bar.complete) {
+          bar.tick(loaded - bar.curr);
+        }
+      }
+    };
+
+    // Upload
     queue.add(function(callback) {
-      var files = fs.readdirSync(argPath);
+
+      var getFilesInDir = function(dir) {
+        var results = []
+        var list = fs.readdirSync(dir)
+        list.forEach(function(file) {
+          file = dir + '/' + file
+          var stat = fs.statSync(file)
+          if (stat && stat.isDirectory()) results = results.concat(getFilesInDir(file))
+          else results.push(file)
+        })
+        return results
+      }
+
+      console.log('Uploading files...');
+      var files = getFilesInDir(argPath);
+      var filesReady = 0;
+      var fileList = [];
       for (var i = 0; i < files.length; i++) {
         if (files[i] != '.DS_Store') {
-          var buffer = fs.readFileSync(argPath + '/' + files[i]);
+          var stream = fs.createReadStream(files[i]);
+          var contentType = mime.lookup(files[i]);
+          var stats = fs.statSync(files[i]);
+          var theFileName = files[i].replace(argPath + '/', '');
 
-          var fileType = files[i];
-          if (files[i].endsWith('.js')) {
-            fileType = 'the JavaScript file';
-          } else if (files[i].endsWith('.css')) {
-            fileType = 'the CSS file';
-          } else if (files[i].endsWith('.html')) {
-            fileType = 'the HTML file';
-          }
+          fileList[i] = {
+            name: theFileName,
+            ready: false,
+            error: false,
+            loaded: 0,
+            total: stats.size,
+          };
 
-          var uploadFn = function(fileName, type) {
-            console.log('Uploading ' + type + '...');
-            s3.putObject({
+          var uploadFn = function(index, fileName, contType, isLast) {
+            s3.upload({
               Bucket: deployOptions.bucket,
               Key: fileName,
-              Body: buffer,
+              Body: stream,
               ACL: 'public-read',
-            }, function(error) {
-              if (error) {
-                console.log('Error uploading ' + type, error);
-              } else {
-                console.log('Successfully uploaded ' + type);
+              ContentType: contType,
+            })
+            .send(function(error, data) {
+              fileList[index].ready = true;
+              if (!error) {
+                fileList[index].loaded = fileList[index].total;
+              }
+
+              printFileProgress(fileList);
+
+              filesReady++;
+              if (filesReady == files.length) {
+                callback();
               }
             });
           };
 
-          uploadFn(files[i], fileType);
+          uploadFn(i, theFileName, contentType, i == files.length - 1);
         }
       }
-
-      callback();
     });
 
     queue.add(function(callback) {
       try { var files = fs.readdirSync(argPath); }
       catch (e) { return; }
-
-      if (files.length > 0)
-        for (var i = 0; i < files.length; i++) {
-          var filePath = argPath + '/' + files[i];
-          if (fs.statSync(filePath).isFile())
-            fs.unlinkSync(filePath);
-          else
-            rmDir(filePath);
+      rmdir(argPath, function(error) {
+        if (error) {
+          console.log('Error removing files', error);
         }
+      });
 
-      fs.rmdirSync(argPath);
+      callback();
+      console.log('App deployed');
     });
 
     queue.run();
